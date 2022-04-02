e231737549b9da2197290b25576341bea6b50a2b
Alapottra <alapottratrojon@gmail.com>
2022-04-02 14:14:27 +0600

    added filtered ingredients

--- src/components/MoreDetails/MoreDetails.js
+++ src/components/MoreDetails/MoreDetails.js
@@ -1,29 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './MoreDetails.css'
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./MoreDetails.css";
 
 const MoreDetails = () => {
+  const { id } = useParams();
+  const [mealDetails, setMealDetails] = useState("");
 
-    const {id} = useParams();
-    const [mealDetails, setMealDetails] = useState('');
-
-    useEffect(() => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-        .then(res => res.json())
-        .then(data => setMealDetails(data.meals[0]));
-    },[id])
-    
-   
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((res) => res.json())
+      .then((data) => setMealDetails(data.meals[0]));
+  }, [id]);
 
 
-    return (
-        <div className='more-details'>
-            <h2 className='recipe-name'>Recipe Name : <span className="colored">{mealDetails.strMeal}</span> </h2>
-            <p className='origin'>Origin: {mealDetails.strArea}</p>
-            
+ const ingredients = Object.values(mealDetails).slice(9,20);
+ 
+ const filtered = ingredients.filter((el) => {
+    return el !== null && typeof el !== 'undefined' && el !== '';
+  });
+  console.log(filtered);
+ 
+  
+  
+
+  return (
+    <div className="more-details">
+
+      <div className="more-details-header">
+
+        <div className="details-img">
+          <img src={mealDetails.strMealThumb} alt={mealDetails.strMeal} />
         </div>
-    );
+        <h2 className="recipe-name">
+          Recipe Name : <span className="colored">{mealDetails.strMeal}</span>{" "}
+        </h2>
+        <p className="origin">Origin: {mealDetails.strArea}</p>
+
+      </div>
+      <div className="ingredients">
+          {filtered.map(ingredient => <span className='ingredient' >{ingredient}</span>)}
+          
+      </div>
+
+
+
+    </div>
+  );
 };
 
 export default MoreDetails;
